608e5cbc3c79cc7f985d76a4e94e67d4982d35d8
Przemek <przemek625@gmail.com>
2017-02-28 19:15:06 +0100

    Merging branch master with branch FR-1

--- reservation/forms.py
+++ reservation/forms.py
@@ -1,3 +1,4 @@
+import datetime
 from django import forms
 
 from reservation.models import Flat, City
@@ -20,14 +21,38 @@ class SearchForm(forms.Form):
             if rsd > red:
                 msg = 'Reservation start date can not be bigger than Reservation end date!'
                 self.add_error('reservation_start_date', msg)
+            if rsd < datetime.date.today():
+                msg = 'Reservation start date can not be lower than today\'s date(%s)!' % datetime.date.today()
+                self.add_error('reservation_start_date', msg)
         except TypeError:
             pass
 
 
 class AddFlatForm(forms.ModelForm):
+    available_from = forms.DateField(
+        widget=forms.DateInput({'class': 'datepicker input-sm form-control', 'placeholder': 'Available from'}),
+        required=True)
+    available_to = forms.DateField(
+        widget=forms.DateInput({'class': 'datepicker input-sm form-control', 'placeholder': 'Available to'}),
+        required=True)
+
     class Meta:
         model = Flat
         fields = '__all__'
+
+    def clean(self):
+        cleaned_data = super(AddFlatForm, self).clean()
+        af = cleaned_data.get('available_from')
+        at = cleaned_data.get('available_to')
+        try:
+            if af > at:
+                msg = 'Available from date can not be bigger than Available to date!'
+                self.add_error('available_from', msg)
+            if af < datetime.date.today():
+                msg = 'Available from date date can not be lower than today\'s date(%s)!' % datetime.date.today()
+                self.add_error('available_from', msg)
+        except TypeError:
+            pass
 
 
 class AddCityForm(forms.ModelForm):
